22f0d78fc1d89f4ee64fbfaec29274d25c55c309
niszogen <43617163+niszogen@users.noreply.github.com>
2020-10-15 21:40:31 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<section>
+<p v-if="like > 0"> 
+  LAJKI {{ like }} 
+</p>
+<p v-else> 
+  ŁO NIE NIKT NIE DAŁ LAJKA 
+</p>
+<button v-on:click="likeC()" type="button" class="btn btn-primary">
+  LAJK
+  </button>
+  </section>
+</template>
+
+<script>
+
+
+export default {
+  data(){
+    return {
+        like: 0,
+    }
+  },
+
+  methods: {
+    likeC() {
+      this.like++;
+    },
+  }
+}
+</script>
